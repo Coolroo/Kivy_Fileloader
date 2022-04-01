cebe56f31565a6da96bce09130650d0cadf16dc2
Coolroo <coolrooswag@gmail.com>
2022-04-01 20:37:15 -0300

    modified:   Controller.py 	modified:   usefulFunctions.py

--- Controller.py
+++ Controller.py
@@ -11,6 +11,15 @@ AUTOSAVE_PATH = os.path.dirname(os.path.realpath(__file__)) + "/AutoSaves/"
 AUTOSAVE_FILE_NAME = "auto"
 
 def checkForExt(filePath, ext):
+    """
+    The checkForExt function checks to see if the file path ends with the specified extension. If it does not, then a new file path is returned with an appended extension.
+    
+    :param filePath: Used to Get the file path of the file that is being checked.
+    :param ext: Used to Check if the file has the correct extension.
+    :return: The filepath if the extension is already correct, or it returns the filepath with an added.
+    
+    :doc-author: Trelent
+    """
     if os.path.splitext(os.path.basename(filePath))[1][1:] != ext:
         return filePath + f'.{ext}'
     else:
@@ -40,6 +49,14 @@ class Controller:
         return self.model.loadedFiles[key]
     
     def doAutoSave(self):
+        """
+        The doAutoSave function saves the current file to a specified location.
+        It also keeps track of the number of files in the directory and deletes them if they exceed a limit.
+        
+        :param self: Used to Access variables that belongs to the class.
+        
+        :doc-author: Trelent
+        """
         if not os.path.exists(AUTOSAVE_PATH):
             print("Path does not exist")
             os.mkdir(AUTOSAVE_PATH)
@@ -50,20 +67,46 @@ class Controller:
         self.autoSave()
 
     def autoSaveFileLimit(self):
+        """
+        The autoSaveFileLimit function checks the number of files in the autosave folder and deletes
+        the oldest file if there are more than a set amount. The maxAutoSaves variable is set to 10, but can be changed
+        in the model class.
+        
+        :param self: Used to Access the class attributes.
+        :return: The number of files in the autosave directory.
+        
+        :doc-author: Trelent
+        """
         numFiles = len([name for name in os.listdir(AUTOSAVE_PATH) if os.path.isfile(os.path.join(AUTOSAVE_PATH, name))])
         if numFiles >= self.model.maxAutoSaves:
-            oldestFile = min([os.path.join(AUTOSAVE_PATH) + file for file in os.listdir(AUTOSAVE_PATH)], key=os.path.getctime)
+            oldestFile = min([os.path.join(AUTOSAVE_PATH) + file for file in os.listdir(AUTOSAVE_PATH) if AUTOSAVE_FILE_NAME in file], key=os.path.getctime)
             os.remove(os.path.join(AUTOSAVE_PATH,oldestFile))
     
     def autoSave(self):
+        """
+        The autoSave function is a timer that runs every self.model.autoSaveInterval seconds and saves the model to disk.
+        
+        :param self: Used to Access the class attributes.
+        :return: A timer object.
+        
+        :doc-author: Trelent
+        """
         t = Timer(self.model.autoSaveInterval, self.doAutoSave)
         t.daemon = True
         t.start()
-        
-    def loadAutosave(self):
-        pass
             
     def addDataSet(self, dataSet):
+        """
+        The addDataSet function adds a new dataset to the model. 
+        It takes one argument, which is the name of the data set. 
+        If this name already exists in either loadedFiles or dataSets, it will print an error message and return 0. Otherwise, it will add that dataset to the dataSets dictionary.
+        
+        :param self: Used to Access variables that belongs to the class.
+        :param dataSet: Used to Specify the name of the dataset.
+        :return: 0 if the dataset is already in the model, else return 1.
+        
+        :doc-author: Trelent
+        """
         if dataSet in self.model.dataSets or dataSet in self.model.loadedFiles:
             print("A dataset species with this name already exists")
             return 0
@@ -72,6 +115,22 @@ class Controller:
             return 1
     
     def loadFile(self, filePath, fileName):
+        """
+        The loadFile function loads a file into the model.
+            Args:
+                filePath (str): The path to the file to be loaded.
+                fileName (str): The name of the new data set created from this function.
+        
+            Returns: 
+                int: 1 if successful, 0 otherwise.
+        
+        :param self: Used to Access the attributes and methods of the class in python.
+        :param filePath: Used to Specify the path to the file that is being loaded.
+        :param fileName: Used to Save the file to a specific name.
+        :return: A 1 if the file is loaded successfully and a 0 if it fails.
+        
+        :doc-author: Trelent
+        """
         if fileName in self.model.loadedFiles or fileName in self.model.dataSets:
             print("A file with this name already exists, please choose another name")
             return 0
@@ -88,6 +147,23 @@ class Controller:
             return 0
         
     def loadExcelSheet(self, filePath, sheetName, fileName):
+        """
+        The loadExcelSheet function loads a sheet from an Excel file into the model.
+           The function takes three arguments: 
+               1) filePath - the path to the Excel file
+               2) sheetName - name of the worksheet in that Excel file
+               3) FileName - what you want to call this dataset in your model.
+        
+           If successful, it returns 1. Otherwise it returns 0.
+        
+        :param self: Used to Access variables that belongs to the class.
+        :param filePath: Used to Specify the location of the file.
+        :param sheetName: Used to Specify the sheet in the excel file that is to be loaded.
+        :param fileName: The index to Store the file in the loadedfiles dictionary.
+        :return: 1 if successful, otherwise 0
+        
+        :doc-author: Trelent
+        """
         if fileName in self.model.loadedFiles or fileName in self.model.dataSets:
             print("A file with this name already exists, please choose another name")
             return 0
@@ -111,9 +187,29 @@ class Controller:
         self.model.dataSets = {}
     
     def getDataSets(self):
+        """
+        The getDataSets function returns a list of DataSet objects.
+        
+        The getDataSets function accepts no arguments and returns the DataSet dictionary.
+        
+        :param self: Used to Access variables that belongs to the class.
+        :return: A dictionary of datasets that are available in the current project.
+        
+        :doc-author: Trelent
+        """
         return self.model.dataSets
 
     def getLoadedDataFrame(self, name):
+        """
+        The getLoadedDataFrame function returns a loaded dataframe from the model. 
+        If the name is not in the list of loaded dataframes, it prints an error message and returns None.
+        
+        :param self: Used to Access the class variables.
+        :param name: Used to Check if the dataframe is already loaded.
+        :return: A dataframe if the name is valid.
+        
+        :doc-author: Trelent
+        """
         if name not in self.model.loadedFiles:
             print("Not a valid dataframe")
             return None
@@ -123,6 +219,17 @@ class Controller:
         return self.model.loadedFiles
     
     def save(self, filePath):
+        """
+        The save function saves the dataframes in the model to a specified HDF File.
+        It takes one argument, which is a string containing the file path and name of 
+        the desired save location. It returns 1 if successful, 0 otherwise.
+        
+        :param self: Used to Refer to the object itself.
+        :param filePath: Used to Specify the path to which the data will be saved.
+        :return: 1 if the save was successful and 0 otherwise.
+        
+        :doc-author: Trelent
+        """
         filePath = checkForExt(filePath, "hdf")
         print(f'Attempting save at {filePath}')
         try:
@@ -143,6 +250,16 @@ class Controller:
             return 0
     
     def getLoadedFile(self, fileName, files=True):
+        """
+        The getLoadedFile function returns the file object of a loaded file. If the file is not loaded, it returns None.
+        
+        :param self: Used to Access variables that belongs to the class.
+        :param fileName: Used to Check if the file has already been loaded.
+        :param files=True: Used to Determine if the function is being used to return a file or not.
+        :return: The Dataframe if files is true, otherwise return the dictionary entry from the loadedfiles dictionary
+        
+        :doc-author: Trelent
+        """
         if fileName in self.model.loadedFiles:
             if files:
                 print("Here I am")
@@ -156,12 +273,31 @@ class Controller:
         return self.model.config.keys()
     
     def getConfigSubUnits(self, unit):
+        """
+        The getConfigSubUnits function returns a list of all the subunits that are configured for a given unit.
+        
+        :param self: Used to Access the class's attributes and methods.
+        :param unit: Used to Specify the unit for which we want to get the configuration.
+        :return: The config of the unit passed in.
+        
+        :doc-author: Trelent
+        """
         if unit in self.model.config:
             return self.model.config[unit]
         else:
             return None
     
     def deleteFile(self, fileName):
+        """
+        The deleteFile function removes a file from the loadedFiles dictionary.
+           If the file is not in the dictionary, it returns 0. Otherwise, it returns 1.
+        
+        :param self: Used to Refer to the object that is calling the function.
+        :param fileName: Used to Delete the file from the loadedfiles dictionary.
+        :return: 1 if successful, otherwise 0.
+        
+        :doc-author: Trelent
+        """
         if fileName in self.model.loadedFiles:
             del self.model.loadedFiles[fileName]
             return 1
@@ -169,6 +305,16 @@ class Controller:
             return 0
 
     def getDataGroup(self, dataSet, dataGroup):
+        """
+        The getDataGroup function returns a dataGroup from the model.
+        
+        :param self: Used to Access variables that belongs to the class.
+        :param dataSet: Used to Specify which dataset to get the datagroup from.
+        :param dataGroup: Used to Specify which datagroup to retrieve from the model.
+        :return: The datagroup for a given dataset.
+        
+        :doc-author: Trelent
+        """
         if dataSet not in self.model.dataSets or dataGroup not in self.model.dataSets[dataSet]:
             print("This datagroup/dataSet does not exist")
             return 0
@@ -176,6 +322,16 @@ class Controller:
             return self.model.dataSets[dataSet][dataGroup]
 
     def deleteDataGroup(self, dataSet, dataGroup):
+        """
+        The deleteDataGroup function deletes a dataGroup from the model.
+        
+        :param self: Used to Reference the class.
+        :param dataSet: Used to Specify which dataset the datagroup is in.
+        :param dataGroup: Used to Specify which datagroup to delete.
+        :return: 1.
+        
+        :doc-author: Trelent
+        """
         if dataSet not in self.model.dataSets:
             print("This dataSet does not exist")
             return 0
@@ -186,6 +342,20 @@ class Controller:
         return 0
 
     def dataToGroup(self, dataSet, dataGroup, subUnit, fileName, measurements, dates):
+        """
+        Imports data from a loaded file into a datagroup
+        
+        :param self: Used to Reference the class instance.
+        :param dataSet: Used to Specify which dataset the datagroup belongs to.
+        :param dataGroup: Used to Specify which datagroup to store the data in
+        :param subUnit: Used to Specify the subunit of the datagroup that is to be used.
+        :param fileName: Used to Specify the name of the file the data came from.
+        :param measurements: Used to Specify which measurements to include in the datagroup.
+        :param dates: Used to Specify which dates to include in the datagroup.
+        :return: 1 if successful, else 0
+        
+        :doc-author: Trelent
+        """
         print(f'dates = {dates}, measurements= {measurements}')
         if dataSet not in self.model.dataSets:
             print("This dataSet does not exist")
@@ -199,6 +369,19 @@ class Controller:
             return 1
 
     def addDataGroup(self, dataSet, dataGroup, Unit):
+        """
+        The addDataGroup function adds a new dataGroup to the specified dataSet.
+        If the specified dataSet does not exist, print an error message and return 0
+        If the specified dataGroup already exists in that dataset, it will print an error message and return 0.
+        
+        :param self: Used to Refer to the object instance itself.
+        :param dataSet: Used to Specify which dataset the new datagroup will be added to.
+        :param dataGroup: Used to Create a new datagroup in the dataset.
+        :param Unit: Used to Specify the unit of measurement for the data in this group.
+        :return: 0 if the method fails, otherwise 1
+        
+        :doc-author: Trelent
+        """
         if dataSet not in self.model.dataSets:
             print("This dataSet does not exist")
             return 0
@@ -211,6 +394,15 @@ class Controller:
         return 1
 
     def getDataGroups(self, dataSet):
+        """
+        The getDataGroups function returns a list of the data groups in the specified dataset.
+        
+        :param self: Used to Access the variables and methods of the class in python.
+        :param dataSet: Used to Specify which dataset to use.
+        :return: A list of the data groups that are associated with a particular dataset.
+        
+        :doc-author: Trelent
+        """
         if dataSet not in self.model.dataSets:
             print("This dataset does not exist")
             return None
@@ -218,6 +410,17 @@ class Controller:
             return self.model.dataSets[dataSet]
 
     def exportDataGroups(self, filePath, dataSet, dataGroups):
+        """
+        The exportDataGroups function exports a DataFrame of the data in the specified data groups.
+        
+        :param self: Used to Reference the class instance (the object) when calling a method.
+        :param filePath: Used to Specify where the file should be saved.
+        :param dataSet: Used to Specify which dataset to export.
+        :param dataGroups: Used to Specify which data groups to export.
+        :return: A dataframe of the specified datagroups from the specified dataset.
+        
+        :doc-author: Trelent
+        """
         filePath = checkForExt(filePath, "xlsx")
         if dataSet not in self.model.dataSets:
             print("This is not a valid DataSet")
@@ -239,6 +442,7 @@ class Controller:
                 dates += realDG["measurements"].keys()
                 columns.append(f'[{dG["name"]}] {realDG["source"]} ({dG["dataGroup"]["unit"]})')
         dates = list(set(dates))
+        #Sort the dates using various formats
         try:
             dates.sort(key = lambda date: datetime.strptime(date, '%Y-%m-%d'))
         except:
--- usefulFunctions.py
+++ usefulFunctions.py
@@ -1,9 +1,27 @@
 import keyword
 
 def isIdentifier(word):
+    """
+    The isIdentifier function returns True if the word is a valid identifier, and False otherwise. 
+    A valid identifier is a string that begins with an alphabetic character or underscore (_), and only consists of alphanumeric characters (i.e., it does not allow punctuation, white space, or special characters).
+    
+    :param word: Used to Check if the word is a valid identifier.
+    :return: A boolean value.
+    
+    :doc-author: Trelent
+    """
     return word.isidentifier() and not keyword.iskeyword(word)
 
 def column_string(n):
+    """
+    The column_string function converts a column number into a string that Excel will recognize as a column name.
+       For example, 1 -> A, 26 -> Z, 27 -> AA, 703 -> AAA.
+    
+    :param n: The row index to be converted
+    :return: The alphabetical column string for an integer.
+    
+    :doc-author: Trelent
+    """
     string = ""
     while n > 0:
         n, remainder = divmod(n - 1, 26)
@@ -11,6 +29,16 @@ def column_string(n):
     return string
 
 def column_string_to_int(col):
+    """
+    The column_string_to_int function converts a column string into an integer.
+       For example, the input 'A' will return 0, while the input 'B' will return 1.
+       The function is intended to be used in conjunction with pandas dataframes and series.
+    
+    :param col: Used to Pass the column string to be converted.
+    :return: The integer representation of a column string.
+    
+    :doc-author: Trelent
+    """
     val = 0
     for i, c in enumerate(reversed(col)):
         val += (ord(c) - 64) * (26 ** i)
